b5e32e5fc4c0d8f2d2a0c4b03bad5ee71e6d3098
mikah13 <anhminhhoang13@gmail.com>
2022-05-29 00:07:47 +0700

    push to github

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme/theme";
+import { ROUTES } from "./utils/routes";
+import Loading from "./components/Loading";
+type AppProps = {
+  children?: React.ReactNode;
+};
 
-function App() {
+const App = ({ children }: AppProps) => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        <Router>
+          <Suspense fallback={<Loading />}>
+            <Routes>
+              {ROUTES.map(({ path, Component }, i) => (
+                <Route key={i} path={path} element={<Component />} />
+              ))}
+            </Routes>
+          </Suspense>
+        </Router>
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/utils/routes.tsx
@@ -0,0 +1,6 @@
+import { lazy } from "react";
+const Home = lazy(() => import("../pages/home"));
+export const ROUTES = [
+  { path: "/", Component: Home },
+  { path: "home", Component: Home },
+];
